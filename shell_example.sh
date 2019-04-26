4d5cbc6755859e175f3d46ab642ff5ae7d73b114
Meng <33242823+zonameng@users.noreply.github.com>
2019-04-26 14:42:07 +0800

    Add files via upload

---
+++ shell_example.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+#===============================================================================================
+#   System Required:  CentOS6.x/7 (32bit/64bit) or Ubuntu
+#   Description:  Install IKEV2 VPN for CentOS and Ubuntu
+#   Author: quericy
+#   Intro:  https://quericy.me/blog/699
+#===============================================================================================
+
+clear
+VER=5.7.0
+echo "#==================================================================================="
+echo "# Install IKEV2 VPN for CentOS6.x/7 (32bit/64bit) or Ubuntu or Debian7/8.*"
+echo "# Intro: https://quericy.me/blog/699"
+echo "#"
+echo "# Author:quericy"
+echo "#"
+echo "# Version:$VER"
+echo "#==================================================================================="
+echo ""
+
+__INTERACTIVE=""
+if [ -t 1 ] ; then
+    __INTERACTIVE="1"
+fi
+#color
+__green(){
+    if [ "$__INTERACTIVE" ] ; then
+        printf '\033[1;31;32m'
+    fi
+    printf -- "$1"
+    if [ "$__INTERACTIVE" ] ; then
+        printf '\033[0m'
+    fi
+}
+
+__red(){
+    if [ "$__INTERACTIVE" ] ; then
+        printf '\033[1;31;40m'
+    fi
+    printf -- "$1"
+    if [ "$__INTERACTIVE" ] ; then
+        printf '\033[0m'
+    fi
+}
+
+__yellow(){
+    if [ "$__INTERACTIVE" ] ; then
+        printf '\033[1;31;33m'
+    fi
+    printf -- "$1"
+    if [ "$__INTERACTIVE" ] ; then
+        printf '\033[0m'
+    fi
+}
+
+
+#install strongswan
+function install_ikev2(){
+    disable_selinux
+    get_system
+    yum_install
+    get_my_ip
+}
+
+
+
+# Disable selinux
+function disable_selinux(){
+if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+fi
+}
+
+# Ubuntu or CentOS
+function get_system(){
+    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        system_str="0"
+    elif  grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        system_str="1"
+    elif  grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        system_str="1"
+    elif  grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        system_str="1"
+    else
+        echo "This Script must be running at the CentOS or Ubuntu or Debian!"
+        exit 1
+    fi
+}
+
+#install strongswan
+function yum_install(){
+    if [ "$system_str" = "0" ]; then
+    yum -y install epel-release
+    yum -y install strongswan
+    else
+    apt-get -y install strongswan
+    fi
+}
+# Get IP address of the server
+function get_my_ip(){
+    echo "Preparing, Please wait a moment..."
+    IP=`curl -s checkip.dyndns.com | cut -d' ' -f 6  | cut -d'<' -f 1`
+    if [ -z $IP ]; then
+        IP=`curl -s ifconfig.me/ip`
+    fi
+}
+
+# Initialization step
+install_ikev2
